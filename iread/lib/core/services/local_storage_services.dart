7752ca5fc3eaa3139949ce0322d8047b3e53a14d
Michael Emad <micheal1444gm@gmail.com>
2024-12-15 13:48:58 +0200

    handle latest books in local storage services

--- iread/lib/core/services/local_storage_services.dart
+++ iread/lib/core/services/local_storage_services.dart
@@ -1,22 +1,23 @@
 import 'dart:io';
 import 'dart:typed_data';
+import 'package:iread/features/home/domain/entities/book_entity.dart';
 import 'package:pdf_render/pdf_render.dart' as pdf_render;
 import 'package:iread/features/home/data/models/book_model.dart';
 import 'package:syncfusion_flutter_pdf/pdf.dart';
 
 class LocalStorageServices {
+  final List<String> _bookExtensions = ['.pdf', '.epub'];
+  final String _directoryPath = '/storage/emulated/0/';
   Future<List<BookModel>> getBooksInDevice() async {
     List<BookModel> books = [];
-    final List<String> bookExtensions = [
-      '.pdf',
-      '.epub'
-    ]; // Add more extensions if needed.
+    // Add more extensions if needed.
 
     // Request storage permissions
     //  if (await Permission.storage.request().isGranted) {
     // Specify the directory to scan (Android-specific)
-    Directory rootDir = Directory('/storage/emulated/0/');
-    books = await _searchBooksInDirectory(rootDir, bookExtensions);
+
+    final Directory rootDir = Directory(_directoryPath);
+    books = await _searchBooksInDirectory(rootDir, _bookExtensions);
 
     //} else {
     //  print("Storage permission denied");
@@ -29,34 +30,34 @@ class LocalStorageServices {
       Directory dir, List<String> extensions) async {
     List<BookModel> books = [];
     // Traverse the directory contents
-    var entities = dir.listSync(recursive: true, followLinks: false);
+    var entities = dir
+        .listSync(recursive: true, followLinks: false)
+        .whereType<File>()
+        .where((file) => _bookExtensions.any((ext) => file.path.endsWith(ext)));
     for (var entity in entities) {
-      if (entity is File) {
-        // Check file extension
-        if (extensions.contains(entity.path.split('.').last.toLowerCase())) {
-          var data = await getPdfMetadata(entity.path);
-          var image = await getPdfCoverImage(entity.path);
-          books.add(BookModel(
-              name: entity.uri.pathSegments.last,
-              bookTitle: data.documentInformation.title,
-              bookAuthor: data.documentInformation.author,
-              bookImage: image,
-              bookPagesCount: data.pages.count,
-              bookLoaclPath: entity.path,
-              size: "${entity.lengthSync()} bytes"));
-        }
-      }
+      // Check file extension
+
+      var data = await _getPdfMetadata(entity.path);
+      var image = await _getPdfCoverImage(entity.path);
+      books.add(BookModel(
+          name: entity.uri.pathSegments.last,
+          bookTitle: data.documentInformation.title,
+          bookAuthor: data.documentInformation.author,
+          bookImage: image,
+          bookPagesCount: data.pages.count,
+          bookLoaclPath: entity.path,
+          size: "${entity.lengthSync()} bytes"));
     }
     return books;
   }
 
-  Future<PdfDocument> getPdfMetadata(String filePath) async {
+  Future<PdfDocument> _getPdfMetadata(String filePath) async {
     File pdfFile = File(filePath);
     List<int> bytes = await pdfFile.readAsBytes();
     return PdfDocument(inputBytes: bytes);
   }
 
-  Future<Uint8List> getPdfCoverImage(String filePath) async {
+  Future<Uint8List> _getPdfCoverImage(String filePath) async {
     final doc = await pdf_render.PdfDocument.openFile(filePath);
     final page = await doc.getPage(1); // Get the first page
     final pageImage = await page.render(
@@ -67,5 +68,47 @@ class LocalStorageServices {
     doc.dispose();
 
     return pageImage.pixels;
+  }
+
+  Future<List<BookEntity>> getLatestBooks({
+    int limit = 10,
+  }) async {
+    List<BookEntity> books = [];
+    final directory = Directory(_directoryPath);
+
+    // Ensure the directory exists
+    if (await directory.exists()) {
+      // List all files and filter by extensions
+      final files = directory
+          .listSync(recursive: true)
+          .whereType<File>()
+          .where(
+              (file) => _bookExtensions.any((ext) => file.path.endsWith(ext)))
+          .toList();
+
+      // Sort files by modification date (newest first)
+      files.sort(
+          (a, b) => b.statSync().modified.compareTo(a.statSync().modified));
+
+      // Take the top `limit` results
+      var entities = files.take(limit).toList();
+
+      for (var entity in entities) {
+        // Check file extension
+
+        var data = await _getPdfMetadata(entity.path);
+        var image = await _getPdfCoverImage(entity.path);
+        books.add(BookModel(
+            name: entity.uri.pathSegments.last,
+            bookTitle: data.documentInformation.title,
+            bookAuthor: data.documentInformation.author,
+            bookImage: image,
+            bookPagesCount: data.pages.count,
+            bookLoaclPath: entity.path,
+            size: "${entity.lengthSync()} bytes"));
+      }
+      return books;
+    } 
+    return books;
   }
 }
--- iread/lib/features/home/data/data_sources/home_local_data_source.dart
+++ iread/lib/features/home/data/data_sources/home_local_data_source.dart
@@ -1,3 +1,6 @@
+import 'package:iread/constants.dart';
+import 'package:iread/core/services/hive_services.dart';
+import 'package:iread/core/services/local_storage_services.dart';
 import 'package:iread/features/home/domain/entities/book_entity.dart';
 import 'package:iread/features/home/domain/entities/book_status_entity.dart';
 
@@ -5,4 +8,26 @@ abstract class HomeLocalDataSource {
   Future<List<BookEntity>> fetchAllBooks();
   Future<List<BookEntity>> fetchLastOpennedBooks();
   Future<List<BookStatusEntity>> fetchContinueReadingBooks();
+}
+
+class HomeLocalDataSourceImplementation extends HomeLocalDataSource {
+  final LocalStorageServices localStorageServices;
+  final HiveServices hiveServices;
+
+  HomeLocalDataSourceImplementation(
+      {required this.localStorageServices, required this.hiveServices});
+  @override
+  Future<List<BookEntity>> fetchAllBooks() {
+    return localStorageServices.getBooksInDevice();
+  }
+
+  @override
+  Future<List<BookStatusEntity>> fetchContinueReadingBooks() {
+    return hiveServices.getBooksStatus(BookStatus.reading);
+  }
+
+  @override
+  Future<List<BookEntity>> fetchLastOpennedBooks() {
+    return localStorageServices.getLatestBooks();
+  }
 }
