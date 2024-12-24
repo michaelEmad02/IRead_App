0e8d3f295ac794e9a915065b4b62f385e14f08cf
Michael Emad <micheal1444gm@gmail.com>
2024-12-24 16:20:49 +0200

    library repo

---
+++ iread/lib/features/library/domain/entities/book_status_entity.dart
@@ -0,0 +1,29 @@
+import 'dart:typed_data';
+
+import 'package:hive/hive.dart';
+
+part 'book_status_entity.g.dart';
+
+@HiveType(typeId: 2)
+class BookStatusEntity extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final Uint8List image;
+  @HiveField(2)
+  final String status;
+  @HiveField(3)
+  final int pagesCount;
+  @HiveField(4)
+  final int pagesReadedCount;
+  @HiveField(5)
+  final String bookPath;
+
+  BookStatusEntity(
+      {required this.status,
+      required this.pagesReadedCount,
+      required this.title,
+      required this.image,
+      required this.pagesCount,
+      required this.bookPath});
+}
---
+++ iread/lib/features/library/domain/repositories/library_repo.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:iread/core/errors/failure.dart';
+import 'package:iread/features/library/domain/entities/book_status_entity.dart';
+
+abstract class LibraryRepo {
+  Future<Either<Failure, List<BookStatusEntity>>> fetchToReadBooks();
+  Future<Either<Failure, List<BookStatusEntity>>> fetchReadingBooks();
+  Future<Either<Failure, List<BookStatusEntity>>> fetchCompletedBooks();
+
+  Future<Either<Failure, List<void>>> addBook({required BookStatusEntity book});
+
+  Future<Either<Failure, List<void>>> updateBook(
+      {required BookStatusEntity book});
+
+  Future<Either<Failure, List<void>>> removeBook(
+      {required BookStatusEntity book});
+}
